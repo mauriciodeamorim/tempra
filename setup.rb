94286717a53a724d5383a29553decc86f5fb5080
Mauricio de Amorim <amorim@timedev.com>
2010-10-29 12:11:42 -0200

    Start configurations

---
+++ setup.rb
@@ -0,0 +1,7 @@
+run "rm -Rf .gitignore README public/index.html public/javascripts/* test public/images/rails.png"
+
+gem 'inherited_resources', '>=1.1.2'
+gem 'will_paginate', '>=3.0.pre2'
+gem 'devise'
+
+
