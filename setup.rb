4febd6d94ed34959545f8bcd887c7b8326eceb4a
Mauricio de Amorim <amorim@timedev.com>
2010-10-29 13:32:54 -0200

    Some tests

--- setup.rb
+++ setup.rb
@@ -1,5 +1,8 @@
 run "rm -Rf .gitignore README public/index.html public/javascripts/* test public/images/rails.png"
 
+gitignore
+
+
 gem 'inherited_resources', '>=1.1.2'
 gem 'will_paginate', '>=3.0.pre2'
 gem 'devise'
